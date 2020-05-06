dd06abf6ef7ea41b393f9888a4a7e0dc36be7a30
master <yinzxpersonal@gmail.com>
2020-05-05 20:40:36 -0700

    make files unhidden

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ ln -sf ~/.dotfiles/vim/vimrc ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sf ~/.dotfiles/vim/package.json ~/.config/coc/extensions/package.json
 ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/.dotfiles/.ctags ~/.ctags
-ln -sf ~/.dotfiles/.gdbinit ~/.gdbinit
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/ctags ~/.ctags
+ln -sf ~/.dotfiles/gdbinit ~/.gdbinit
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 
 # install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
