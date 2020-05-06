22f0bdf0962b1db536a021b182da23f84327cba1
master <yinzxpersonal@gmail.com>
2020-05-05 20:36:00 -0700

    change directories

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/vim/vimrc ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf ~/.dotfiles/vim/package.json ~/.config/coc/extensions/package.json
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/.ctags ~/.ctags
 ln -sf ~/.dotfiles/.gdbinit ~/.gdbinit
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -sf ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
-ln -sf ~/.dotfiles/package.json ~/.config/coc/extensions/package.json
 
-./brew.sh
+# install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install brew packages
+sudo ./brew.sh
